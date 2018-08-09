766e9d94181c752e97af5d07562a1a3db13745a6
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-08-08 17:58:59 -0700

    Add alertMe() that logs the input value and clears the input field.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ new Vue({
 	data: {
 		counter: 0,
 		x: 0,
-		y: 0
+		y: 0,
+		inputPlaceholder: 'enter text'
 	},
 	methods: {
 		increase: function(num, event) {
@@ -12,6 +13,10 @@ new Vue({
 		updateCoordinates: function(event) {
 			this.x = event.clientX;
 			this.y = event.clientY;
+		},
+		alertMe: function(event) {
+			console.log(event.target.value);
+			event.target.value = '';
 		}
 	}
 });
